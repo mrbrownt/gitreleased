26fafe1dcf901bb8897426f9ceb9f41f9c5071cf
Todd Brown <todd@proceedfinance.com>
2019-06-19 15:33:17 -0500

    Revert "Try directly using gitlab image on GCR"
    This reverts commit 723d28c844538035ed1c2b0614bfaf2a2af1407b.
    
    Google Cloud Run needs to pull images from google container registry…
    
    Signed-off-by: Todd Brown <todd@proceedfinance.com>

--- pipelines.sh
+++ pipelines.sh
@@ -82,12 +82,17 @@ deploy() {
         setupGitlabDocker
         setupGCP
 
-        GITLAB_IMAGE="${CI_REGISTRY}/${CI_PROJECT_PATH}/auth:${CI_COMMIT_SHA}"
+        GCR_IMAGE="us.gcr.io/spheric-subject-165900/gitreleased/auth:${CI_COMMIT_REF_NAME}"
+
+        GITLAB_TAG="${CI_REGISTRY}/${CI_PROJECT_PATH}/auth:${CI_COMMIT_SHA}"
+        docker pull "${GITLAB_TAG}"
+        docker tag "${GITLAB_TAG}" "${GCR_IMAGE}"
+        docker push "${GCR_IMAGE}"
 
         gcloud beta run deploy gitreleased-auth \
             --project spheric-subject-165900 \
             --region us-central1 \
-            --image "${GITLAB_IMAGE}" \
+            --image "${GCR_IMAGE}" \
             --set-env-vars "GITHUB_KEY=${GITHUB_KEY},GITHUB_SECRET=${GITHUB_SECRET},GITLAB_USER=mrbrownt,GITLAB_ACCESS_TOKEN=${GITLAB_ACCESS_TOKEN},ENVIRONMENT=production,SESSION_SECRET=${SESSION_SECRET},CLOUDSQL=yes,DB_HOST=spheric-subject-165900:us-central1:gitreleased,DB_PASS=${DB_PASS}" \
             --set-env-vars BASE_URL=auth.gitreleased.app
         ;;
@@ -95,12 +100,17 @@ deploy() {
         setupGitlabDocker
         setupGCP
 
-        GITLAB_IMAGE="${CI_REGISTRY}/${CI_PROJECT_PATH}/backend:${CI_COMMIT_SHA}"
+        GCR_IMAGE="us.gcr.io/spheric-subject-165900/gitreleased/backend:${CI_COMMIT_REF_NAME}"
+
+        GITLAB_TAG="${CI_REGISTRY}/${CI_PROJECT_PATH}/backend:${CI_COMMIT_SHA}"
+        docker pull "${GITLAB_TAG}"
+        docker tag "${GITLAB_TAG}" "${GCR_IMAGE}"
+        docker push "${GCR_IMAGE}"
 
         gcloud beta run deploy gitreleased-backend \
             --project spheric-subject-165900 \
             --region us-central1 \
-            --image "${GITLAB_IMAGE}" \
+            --image "${GCR_IMAGE}" \
             --set-env-vars "GITHUB_KEY=${GITHUB_KEY},GITHUB_SECRET=${GITHUB_SECRET},GITLAB_USER=mrbrownt,GITLAB_ACCESS_TOKEN=${GITLAB_ACCESS_TOKEN},ENVIRONMENT=production,SESSION_SECRET=${SESSION_SECRET},CLOUDSQL=yes,DB_HOST=spheric-subject-165900:us-central1:gitreleased,DB_PASS=${DB_PASS}"
         ;;
     esac
